3d4cb692837c2de5eb0fb6720b8cc1bd6e911669
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-01-23 17:37:40 +0100

    Create week1.py

---
+++ week1.py
@@ -0,0 +1,11 @@
+match = []
+
+h = {"Atlanta": ["Xavier", "Zeus", "Yam"], 
+     "Boston": ["Yam", "Xavier", "Zues"], 
+     "Detroit": ["Zeus", "Yam", "Xavier"]}
+
+s = {"Xavier": ["Boston", "Atlanta", "Detroit"],
+     "Yam": ["Atlanta", "Detroit", "Boston"],
+     "Zues": ["Detroit", "Boston", "Atlanta"]}
+
+while elem in h.values = []:
